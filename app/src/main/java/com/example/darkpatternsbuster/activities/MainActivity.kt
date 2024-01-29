76c928007d0fef2e611b80ade62ffa1e1dfcb9f6
Aritra <aritrabiswas9788@gmail.com>
2024-01-29 15:05:50 +0530

    Service class tested

--- app/src/main/java/com/example/darkpatternsbuster/activities/MainActivity.kt
+++ app/src/main/java/com/example/darkpatternsbuster/activities/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.darkpatternsbuster.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.darkpatternsbuster.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var checkWeb: CheckBox
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkWeb = findViewById(R.id.WebCheck)
+        checkWeb.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setMessage("Automating Messages is riskier due to WhatsApp security policies. \nThis involves a process where every selected contact will be opened and the message will be sent one by one over a period of time. \nThis requires an additional permission known as Accessibility. \nDo you still want to enable this setting?")
+            builder.setTitle("DISCLAIMER")
+            builder.apply {
+                setPositiveButton("YES") { dialog, id ->
+                    showToast("Redirects to accessibility")
+                    //createAlertDialog("Navigate To the Installed apps section and enable My WhatsApp Accessibility for WhatsApp Automation to work.","STEPS for Accessibility")
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+                    builder.setMessage("Navigate To the Installed apps section and enable My WhatsApp Accessibility for WhatsApp Automation to work.")
+                    builder.setTitle("STEPS for Accessibility")
+                    builder.apply {
+                        setPositiveButton("OK") { dialog, id ->
+                            val intentAccess = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+                            intentAccess.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intentAccess)
+                        }
+                    }
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
+
+                }
+                setNegativeButton("NO") { dialogInterface: DialogInterface, i: Int ->
+                    checkWeb.isChecked = false
+                }
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+    }
+    private fun showToast(message:String)
+    {
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/darkpatternsbuster/services/WebsiteAnalyzerService.kt
+++ app/src/main/java/com/example/darkpatternsbuster/services/WebsiteAnalyzerService.kt
@@ -1,20 +1,77 @@
 package com.example.darkpatternsbuster.services
 
 import android.accessibilityservice.AccessibilityService
-import android.app.Service
 import android.content.Intent
-import android.os.IBinder
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+
 
 class WebsiteAnalyzerService : AccessibilityService() {
 
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        if(rootInActiveWindow == null)
+        if (rootInActiveWindow == null)
             return
         val rootNodeInfo: AccessibilityNodeInfoCompat = AccessibilityNodeInfoCompat.wrap(rootInActiveWindow)
+        //Log.i("ServiceChecker", "Accessibility Event Triggered")
+        if (event != null) {
+            if (AccessibilityEvent.eventTypeToString(event.eventType).contains("WINDOW")) {
+                val nodeInfo = event.source?.getChild(0)?.getChild(0)
+                Log.i("ServiceChecker", nodeInfo?.className.toString())
+                //nodeDfs(nodeInfo)
+                //getUrlsFromViews(nodeInfo)
+                if(nodeInfo!=null && nodeInfo.text!=null) {
+                    val x = nodeInfo.text.toString()
+                    Log.i("ServiceChecker", x)
+                }
+                else
+                    if(nodeInfo?.text==null)
+                         Log.i("ServiceChecker", "null text Found")
+                    else
+                        Log.i("ServiceChecker", "null root Found")
+            }
+        }
+    }
 
+    fun nodeDfs(info: AccessibilityNodeInfo?) {
+        if (info == null) return
+        if (info.text != null && info.text.length > 0) {
+
+            val x = (info.text.toString() + " class: " + info.className)
+            Log.i("ServiceChecker", x)
+        }
+        for (i in 0 until info.childCount) {
+            val child = info.getChild(i)
+            nodeDfs(child)
+            child?.recycle()
+        }
+    }
+
+    fun getUrlsFromViews(info: AccessibilityNodeInfo?) {
+        try {
+            if (info == null) return
+            if (info.text != null && info.text.isNotEmpty()) {
+                val capturedText = info.text.toString()
+                if (capturedText.contains("https://") || capturedText.contains("http://")) {
+
+
+                        val x = (info.text.toString() + " class: " + info.className)
+                        Log.i("ServiceChecker", x)
+
+                }
+            }
+            for (i in 0 until info.childCount) {
+                val child = info.getChild(i)
+                getUrlsFromViews(child)
+                child?.recycle()
+            }
+        } catch (ex: StackOverflowError) {
+            ex.printStackTrace()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 
     override fun onInterrupt() {
@@ -27,5 +84,6 @@ class WebsiteAnalyzerService : AccessibilityService() {
 
     override fun onServiceConnected() {
         super.onServiceConnected()
+        Log.i("ServiceChecker", "Service-Connected")
     }
 }
